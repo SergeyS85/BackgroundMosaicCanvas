8cf2f2537fa6320c4bc8320176d1307205961035
SergeyS85 <surzhiks.85@mail.ru>
2022-01-06 21:40:19 +0300

    change bg

--- canvas.js
+++ canvas.js
@@ -5,9 +5,9 @@ let h       = canvas.height = innerHeight;
 let arrImg  = []
 let copyArrImg = []
 
-const SQRT_PIECES_IMG = 30
+const SQRT_PIECES_IMG = 20
 let bgImage = new Image();
-bgImage.src = 'treugolniki_fon_svet.jpg'
+bgImage.src = 'siluet_chelovek_anonim.jpg'
 
 function pushArrImg(){
 	for(let i = 0;i <= h;i += SQRT_PIECES_IMG+1){
@@ -31,34 +31,52 @@ function createImage(){
 		  copyArrImg[i].hImg)
 	}
 }
+// ctx.imageSmoothingEnabled = true
+// ctx.imageSmoothingQuality = "low"
 
-function loop(){
-////////////////////
-  ctx.clearRect(0,0,w,h)
-	  createImage()
-	requestAnimationFrame(loop)
-///////////////////
+let gravity = false
+
+function gravityDown(){
+	let ss = 0
+	if(gravity){
+		copyArrImg.forEach(function(e){
+		  e.sy += 0.01*ss++
+	  })
+	}
 }
-
+canvas.addEventListener('click',()=>{
+	gravity = true
+})
 function parallax(){
 	canvas.addEventListener('mousemove',function(event){
 	  let distance = 0
 	  let center = SQRT_PIECES_IMG/2
 	  copyArrImg.forEach(function(el,index){
       distance = Math.sqrt(((el.sx+center - event.x)*(el.sx+center - event.x))+((el.sy+center - event.y)*(el.sy+center - event.y))) // Найди отличия)))по факту формула нахождения длины гепатинузы(ею является distence)
-      if(distance < 100){
+      if(distance < 120){
       	if(event.x < el.sx) el.sx+= 8
       	if(event.x > el.sx) el.sx-= 5
       	if(event.y < el.sy) el.sy+= 12
       	if(event.y > el.sy) el.sy-= 9
+      		// el.wImg = 50
+      		// el.hImg = 50
+      		// ctx.rotate(45 * Math.PI / 180)
       }
-      if(distance > 500){
+      if(distance > 700){
       	el.sx = arrImg[index].sx
       	el.sy = arrImg[index].sy
       }
     })
   }) 
 
+}
+function loop(){
+////////////////////
+  ctx.clearRect(0,0,w,h)
+	  createImage()
+	  gravityDown()
+	requestAnimationFrame(loop)
+///////////////////
 }
 function initAllFunction(){
 	pushArrImg()	
